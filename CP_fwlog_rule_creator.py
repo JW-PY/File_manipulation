7f406ee0541e410c44685e1a6e840f5424ec362e
JW-PY <woodze@internode.on.net>
2016-05-01 19:48:00 +0930

    Update CP_fwlog_rule_creator.py

--- CP_fwlog_rule_creator.py
+++ CP_fwlog_rule_creator.py
@@ -4,8 +4,10 @@ import csv
 directory = 'C:\Python27\my_scripts'
 os.chdir(directory)
 
-file = "sgsgvoges002.2015-09-27_001827_732.log.txt"
-file2 = "sgsgvoges002.2015-09-27_001827_732.log.txt"
+file  = "sgsgvoges002.2015-09-27_001827_732.log.txt"
+file_tmp = "TEMP.log.txt"
+file1 = "sgsgvoges002.2015-09-27_001827_732-EDITED.log.txt"
+file2 = "CP_FW_log1_header.csv"
 file3 = 'CP_FW_log1.csv'
 file4 = 'CP_FW_log1_row_strip.csv'
 file5 = 'CP_FW_log1_unique.csv'
@@ -20,14 +22,24 @@ def line_count(x):
             a = a + 1
     print a
 
-def preview_file():
+def preview_file(x):
     a = 0
     # if a < 6:
-    with open(file, 'rb') as f:
+    with open(x, 'rb') as f:
         for line in f:
             a = a + 1
             if a < 6:
                print line.strip()
+			   
+def preview_file2(x):
+    a = 0
+    # if a < 6:
+    with open(x, 'rb') as f:
+        for line in f:
+            a = a + 1
+            if a < 60000000:
+                if 's_port: 59939' in line:
+                    print line.strip()
 			   
 def delete_working_files():
     try:
@@ -48,34 +60,62 @@ def delete_working_files():
 	    pass
 		
 
-def convert_semicolon_to_comma():
+def clean_file():
+    a = ('\n')
+    f1 = open(file, 'r')
+    f2 = open(file_tmp, 'w')
+    for line in f1:
+       f2.write(line.replace('; src', ' src'))
+    f1.close()
+    f2.close()
+    f1 = open(file_tmp, 'r')
+    f2 = open(file1, 'w')
+    for line in f1:
+       f2.write(line.replace(' src', '; src'))
+    f1.close()
+    f2.close()
+    try:
+        os.remove ('TEMP.log.txt')
+    except:
+	    pass
+
+def add_headings_to_csv():
+    with open(file2, 'wb') as outcsv:
+        writer = csv.writer(outcsv)
+        writer.writerow(["Date time accept-deny fw name Interface-Service ID", "source", "destination", "protocol", "rule", "product", "service", "source port", "product_family"])					
+
+def convert_comma_to_semicolon():
     a = ('\n')
     f1 = open(file2, 'r')
-    f2 = open('file3-comma.txt', 'a')
+    f2 = open(file3, 'w')
     for line in f1:
-       f2.write(line.replace(';', ','))
+       f2.write(line.replace(',', ';'))
     f1.close()
     f2.close()
 
-def add_headings_to_csv():
-    with open('CP_FW_log1.csv', 'wb') as outcsv:
-        writer = csv.writer(outcsv)
-        writer.writerow(["Date time accept-deny fw name", "Interface-Service ID", "source", "destination", "protocol", "rule", "product", "service", "source port", "product_family"])					
-
 def import_fwlog_in_csv():
-    with open('file3-comma.txt', 'rb') as f:
+    with open(file1, 'rb') as f:
         for line in f:
-            with open("CP_FW_log1.csv", "a") as myfile:
-                if 'accept' in line:
+            with open(file3, "a") as myfile:
+                if 'accept CN=AUSYZVFOGES002_AUSYZFVOGES005' in line:
                     l = line.strip()
                     myfile.writelines(l+'\n')
 
+def remove_comma():
+    a = ('\n')
+    f1 = open(file2, 'r')
+    f2 = open(file3, 'w')
+    for line in f1:
+       f2.write(line.replace(',', ';'))
+    f1.close()
+    f2.close()
+
 def strip_columns():
     with open(str(file3)) as csvfile:
-        reader = csv.DictReader(csvfile)
+        reader = csv.DictReader(csvfile, delimiter=';')
         with open(file4, "a") as myfile:
             for row in reader:
-                a = (row['source'], row['destination'], row['service'])
+                a = (row['source'], row['destination'], row['protocol'], row['service'])
                 myfile.write (str(a)+"\n")
 
 			
@@ -89,12 +129,15 @@ def unique_rows():
 			
 
 #line_count()
-#preview_file()
+#preview_file('file3-comma.txt')
+#preview_file2('file3-comma.txt')
 delete_working_files()
-convert_semicolon_to_comma()
+clean_file()
 add_headings_to_csv()
+convert_comma_to_semicolon()
 import_fwlog_in_csv()
 strip_columns()
 unique_rows()
 line_count(file2)
 line_count(file5)
+
